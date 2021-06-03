bb54ff93903ffbd6625999670ca8ed53caf6c4d7
ChrisW0524 <chriswang20050524@gmail.com>
2021-06-03 22:01:00 +0800

    Implemented line chart

--- static/js/api.js
+++ static/js/api.js
@@ -8,12 +8,19 @@ async function fetchRequest(url){
 const api_links = {
     summary(){
         return covid_api_url + 'summary'
+    },
+    countryData(country, status){
+        console.log(covid_api_url + 'country/' + country + '/status/' + status)
+        return covid_api_url + 'country/' + country + '/status/' + status
     }
 }
 
 const covidApi = {
     async getSummary(){
         return await fetchRequest(api_links.summary())
+    },
+    async getCountryData(country, status){
+        return await fetchRequest(api_links.countryData(country, status))
     }
 }
 
--- static/js/data.js
+++ static/js/data.js
@@ -4,29 +4,26 @@ const COLORS = {
     deaths: '#373c43',
 }
 
-const CASE_STATUS = {
-    confirmed: 'confirmed',
-    recovered: 'recovered',
-    deaths: 'deaths',
-}
-
 let body = document.querySelector('body')
 let all_time_chart, recover_rate_chart, death_rate_chart, population_chart
 
 window.onload = async () => {
     console.log('ready...')
     /* Input for location of data */
-    loadData('Switzerland')
+    let location = 'south-africa'
+    console.log(location.Slug)
+    loadData(location)
 }
 
-async function loadSummary(country){
+async function loadSummary(country) {
     let summaryData = await covidApi.getSummary()
     console.log(summaryData)
     let summary = summaryData.Global
-    if (!(country === 'Global')){
-        summary = summaryData.Countries.filter(e => e.Country === country)[0]
+    if (!(country === 'Global')) {
+        summary = summaryData.Countries.filter(e => e.Slug === country)[0]
     }
     console.log(summary);
+
     showTotalConfirmed(summary.TotalConfirmed)
     showTotalRecovered(summary.TotalRecovered)
     showTotalDeaths(summary.TotalDeaths)
@@ -35,30 +32,108 @@ async function loadSummary(country){
     showNewDeaths(summary.NewDeaths)
 }
 
-async function loadData(country){
-    await loadSummary(country)
+async function loadCountryData(country) {
+    const confirmed = await covidApi.getCountryData(country, 'confirmed')
+    const recovered = await covidApi.getCountryData(country, 'recovered')
+    console.log(recovered)
+    const deaths = await covidApi.getCountryData(country, 'deaths')
+    let countryData = {
+        cases: [],
+        recovered: [],
+        deaths: [],
+        dates: []
+    }
+    confirmed.forEach(element => {
+        countryData.cases.push(element.Cases)
+        countryData.dates.push(element.Date.substr(0, 10))
+    });
+    recovered.forEach(element => {
+        countryData.recovered.push(element.Cases)
+    });
+    deaths.forEach(element => {
+        countryData.deaths.push(element.Cases)
+    });
+    console.log(countryData)
+    return countryData
 }
 
-function showTotalConfirmed(data){
+async function loadData(country) {
+    await loadSummary(country)
+    await initializeLineChart(country);
+}
+
+async function initializeLineChart(country) {
+    let data = await loadCountryData(country)
+    var ctx = document.getElementById('line_chart').getContext('2d');
+    var line_chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data.dates,
+            datasets: [{
+                label: 'Confirmed',
+                data: data.cases,
+                fill: false,
+                borderColor: 'rgb(255, 0, 0)',
+                tension: 0.1
+              },
+              {
+                label: 'Recovered',
+                data: data.recovered,
+                fill: false,
+                borderColor: 'rgb(0, 128, 0)',
+                tension: 0.1
+              },
+              {
+                label: 'Deaths',
+                data: data.deaths,
+                fill: false,
+                borderColor: 'rgb(55, 60, 67)',
+                tension: 0.1
+              }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            elements: {
+                point: {
+                    radius: 0
+                }
+            },
+            layout: {
+                padding: {
+                    left: 25,
+                    right: 25
+                }
+            }
+        }
+    });
+}
+
+/* update html */
+function showTotalConfirmed(data) {
     document.querySelector('#total_confirmed').textContent = data
 }
 
-function showTotalRecovered(data){
+function showTotalRecovered(data) {
     document.querySelector('#total_recovered').textContent = data
 }
 
-function showTotalDeaths(data){
+function showTotalDeaths(data) {
     document.querySelector('#total_deaths').textContent = data
 }
 
-function showNewConfirmed(data){
+function showNewConfirmed(data) {
     document.querySelector('#new_confirmed').textContent = data
 }
 
-function showNewRecovered(data){
+function showNewRecovered(data) {
     document.querySelector('#new_recovered').textContent = data
 }
 
-function showNewDeaths(data){
+function showNewDeaths(data) {
     document.querySelector('#new_deaths').textContent = data
 }
