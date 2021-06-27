dda0ea404a0dd53e02c22492f783ae3384953e97
Reversal9 <jonnenc@gmail.com>
2021-06-26 19:08:21 -0500

    adjust zoom

--- static/js/map.js
+++ static/js/map.js
@@ -120,11 +120,11 @@ async function initMap() {
 
 	//values = values.sort((a, b) => a - b);
 
-	var map = L.map('map').setView([0, 0], 1);
+	var map = L.map('map').setView([0, 0], 3);
 	L.tileLayer('https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=9Nvo0KiD7dG0zZ8NTVtl', {
 		tileSize: 512,
 		zoomOffset: -1,
-		minZoom: 1,
+		minZoom: 2,
 		attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
 		crossOrigin: true,
 		noWrap: true,
